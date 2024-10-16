211f8463cef8901462dfa886d9eb79f564870ddb
Timurbek <timurbekbeksaburov.com>
2024-10-16 13:48:20 +0500

    microservice

---
+++ category/src/app.ts
@@ -0,0 +1,23 @@
+import { Category, CategoryModule } from '@modules';
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host:'localhost',
+      port:5432,
+      username:'postgres',
+      password:'Timurbek123@',
+      database:'myblog',
+      synchronize:true,
+      autoLoadModels:true,
+      sync:{force:true}
+    }),
+    CategoryModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
